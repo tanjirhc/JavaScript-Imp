bb3224ac30a084381ee00015316769a349a85da7
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-07 06:20:09 +0600

    promise

---
+++ promise/promise.js
@@ -0,0 +1,20 @@
+console.log("welcome");
+
+// how to create a promise- pending, resolve, reject
+const promise1 = new Promise((resolve, reject) =>{
+
+  let completedPromise = true;
+  if(completedPromise) {
+    resolve("completed promise 1")
+  } else{
+    reject("not completed promise 1")
+  }
+
+})
+
+// console.log(promise1);
+
+promise1.then(res => {
+  console.log(res);
+})
+console.log("end");
