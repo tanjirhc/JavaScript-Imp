482279106bd50b305cef7f38ec15884d8d307cf3
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-07 07:29:21 +0600

    task moved to function

--- async/async.js
+++ async/async.js
@@ -1,6 +1,22 @@
 // synchronous programming
-console.log('Task1');
-console.log('Task2');
-console.log('Task3');
-console.log('Task4');
-console.log('Task5');
+const taskOne = () =>{
+  console.log('Task1');
+}
+const taskTwo = () =>{
+  console.log('Task2');
+}
+const taskThree = () =>{
+  console.log('Task3');
+}
+const taskFour = () =>{
+  console.log('Task4');
+}
+const taskFive = () =>{
+  console.log('Task5');
+}
+
+taskOne()
+taskTwo()
+taskThree()
+taskFour()
+taskFive()
