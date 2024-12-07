abc940e6e53244d0397c1998fb9aaf8f5c73e2e1
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-07 06:48:31 +0600

    promise chaining

---
+++ promise/promise2.js
@@ -0,0 +1,23 @@
+const taskOne = () => {
+  return new Promise((resolve, reject) =>{
+    resolve('task 1 is completed')
+  })
+}
+const taskTwo = () => {
+  return new Promise((resolve, reject) =>{
+    resolve('task 2 is completed')
+  })
+}
+const taskThree = () => {
+  return new Promise((resolve, reject) =>{
+    resolve('task 3 is completed')
+  })
+}
+const taskFour = () => {
+  return new Promise((resolve, reject) =>{
+    resolve('task 4 is completed')
+  })
+}
+
+taskOne
+.then((res) => console.log(res))
