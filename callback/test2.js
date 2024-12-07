54c6cf69a2fb6d8bbf1c47d9df153ddbbfb4d4c3
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-07 17:23:13 +0600

    passing callback in taskOne

--- callback/test2.js
+++ callback/test2.js
@@ -1,14 +1,23 @@
 // callback and higher order function
 
-function square(x){
-  console.log(`square of ${x}: ${x * x}`);
+const taskOne = (callback) =>{
+  console.log('Task1');
+  callback()
 }
 
-// const y = square
-// y(5)
-
-function higherOrderFunction(num, callback) {
-  callback(num)
+const taskTwo = () =>{
+  setTimeout(() => {
+    console.log('Task2. Data Loading');
+  }, 3000)
+}
+const taskThree = () =>{
+  console.log('Task3');
+}
+const taskFour = () =>{
+  console.log('Task4');
+}
+const taskFive = () =>{
+  console.log('Task5');
 }
 
-higherOrderFunction(6, square)
+taskOne()
