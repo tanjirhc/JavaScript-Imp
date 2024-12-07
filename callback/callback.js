8bb9b5f75540b1566afd73d241665ca4f1fc84f7
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-07 17:06:12 +0600

    callback with anonymous function

---
+++ callback/callback.js
@@ -0,0 +1,25 @@
+// synchronous programming
+const taskOne = () =>{
+  console.log('Task1');
+}
+
+const taskTwo = () =>{
+  setTimeout(() => {
+    console.log('Task2. Data Loading');
+  }, 2000)
+}
+const taskThree = () =>{
+  console.log('Task3');
+}
+const taskFour = () =>{
+  console.log('Task4');
+}
+const taskFive = () =>{
+  console.log('Task5');
+}
+
+taskOne()
+taskTwo()
+taskThree()
+taskFour()
+taskFive()
