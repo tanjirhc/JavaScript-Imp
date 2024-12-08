3abefeb3b0c365b66095358d2b93ac9e4f24ca32
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-08 23:40:36 +0600

    POST method fetch api

--- fetchAPI/fetch.js
+++ fetchAPI/fetch.js
@@ -1,7 +1,7 @@
 console.clear()
 
-const makeRequest = async (url) =>{
-  const res = await fetch(url);
+const makeRequest = async (url, config) =>{
+  const res = await fetch(url, config);
   if(!res.ok){
     const message = `Error : ${res.status}`
     throw new Error(message)
@@ -10,10 +10,29 @@ const makeRequest = async (url) =>{
   return data
 }
 
-const getData = () =>{
-  makeRequest('https://jsonplaceholder.typicode.com/posts')
+const sendData = () =>{
+  makeRequest('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+  })
+  
   .then((res) => console.log(res))
   .catch((err) => console.log(err))
 }
 
-getData()
+sendData()
+
+// const getData = () =>{
+//   makeRequest('https://jsonplaceholder.typicode.com/posts')
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err))
+// }
+
+// getData()
