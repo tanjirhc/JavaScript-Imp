3866347c0f487959e5612ace9302989fc11b6164
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-08 19:02:19 +0600

    error handling

--- fetchAPI/fetch.js
+++ fetchAPI/fetch.js
@@ -1,4 +1,11 @@
 console.clear()
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((res) => res.json())
+fetch('https://jsonplaceholder.typicode.com/posts/101')
+  .then((res) => {
+    if(!res.ok){
+      const message = `Error: ${res.status}`
+      throw new Error (message)
+    }
+    return res.json()
+  })
   .then((res) => console.log(res))
+  .catch((err) => console.log(err))
