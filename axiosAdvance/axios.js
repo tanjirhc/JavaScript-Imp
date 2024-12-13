373108d045e2fe624e0b402f451fb719f9f2e5b4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-13 22:45:48 +0600

    using as function

--- axiosAdvance/axios.js
+++ axiosAdvance/axios.js
@@ -1,6 +1,14 @@
 console.clear()
 
-axios
-  .delete('https://jsonplaceholder.typicode.com/posts/1')
-  .then((res) => console.log(res.data))
-  .catch((err) => console.log(err))
+const makeRequest = (config) => {
+
+  return axios(config)
+}
+
+const getData = () => {
+  makeRequest('https://jsonplaceholder.typicode.com/posts')
+  .then((res) =>  console.log(res)  )
+  .catch((err) =>  console.log(err)  )
+}
+
+getData()
