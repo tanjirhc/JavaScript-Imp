2da116cf8d7b2d30622d21dd327b66bf0463e29f
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-13 22:54:03 +0600

    deleteData

--- axiosAdvance/axios.js
+++ axiosAdvance/axios.js
@@ -5,21 +5,31 @@ const makeRequest = async (config) => {
   return await axios(config)
 }
 
-const updateData = () => {
+const deleteData = () => {
   makeRequest({
     url : 'https://jsonplaceholder.typicode.com/posts/1',
-    method: 'put',
-    data : JSON.stringify({
-      id: 1,
-      title: 'fooma',
-      body: 'barma',
-      userId: 1,
-    }),
+    method: 'delete'
   })
   .then((res) =>  console.log(res.data)  )
   .catch((err) =>  console.log(err)  )
 }
-updateData()
+deleteData()
+
+// const updateData = () => {
+//   makeRequest({
+//     url : 'https://jsonplaceholder.typicode.com/posts/1',
+//     method: 'put',
+//     data : JSON.stringify({
+//       id: 1,
+//       title: 'fooma',
+//       body: 'barma',
+//       userId: 1,
+//     }),
+//   })
+//   .then((res) =>  console.log(res.data)  )
+//   .catch((err) =>  console.log(err)  )
+// }
+// updateData()
 
 // const createData = () => {
 //   makeRequest({
