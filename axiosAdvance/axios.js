e8ffa7bbfe834cbd22844ab1ba5cd7717d663f74
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-13 22:32:44 +0600

    error handling

--- axiosAdvance/axios.js
+++ axiosAdvance/axios.js
@@ -1,5 +1,6 @@
 console.clear()
 
 axios
-  .get('https://jsonplaceholder.typicode.com/posts/1')
-  .then((res) => console.log(res))
+  .get('https://jsonplaceholder.typicode.com/posts/101')
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err))
